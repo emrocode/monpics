ad9ba32c9aab817c5094ab914910bda351dfe73f
emrocode <92260849+emrocode@users.noreply.github.com>
2024-05-31 17:46:17 -0400

    Add more types

--- src/types.d.ts
+++ src/types.d.ts
@@ -2,3 +2,14 @@ export type PreviewProps = {
   file: string;
   canvasRef: React.RefObject<HTMLCanvasElement>;
 };
+
+export type EditorProps = {
+  handleTextChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  align: string;
+  setAlign: (align) => void;
+};
+
+export type ThemeContextType = {
+  isDarkMode: boolean;
+  toggleDarkMode: () => void;
+};
