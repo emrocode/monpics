0d2aefcf6ae11673fc5c4a53fcefe52469c8a6bb
emrocode <92260849+emrocode@users.noreply.github.com>
2024-06-14 13:16:49 -0400

    Create card component

---
+++ src/components/Card.tsx
@@ -0,0 +1,36 @@
+import type { CardProps } from "../types";
+
+export default function Card({
+  image,
+  imageFullSize,
+  author,
+  handleClick,
+}: CardProps) {
+  return (
+    <article
+      className="flex cursor-pointer flex-col gap-4 rounded-xl border p-4"
+      onClick={handleClick}
+      data-value={imageFullSize}
+    >
+      <figure className="relative h-[160px] overflow-clip rounded-lg">
+        <img
+          decoding="async"
+          src={image}
+          alt={`MonPics demo image by ${author.name}`}
+          className="block h-full w-full object-cover object-center"
+        />
+      </figure>
+      <p className="text-sm">
+        Image by{" "}
+        <a
+          href={author.profileUrl}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-sky-500 underline hover:bg-sky-500/25"
+        >
+          {author.name}
+        </a>
+      </p>
+    </article>
+  );
+}
