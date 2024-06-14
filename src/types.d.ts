35c54f234673451c4972315d9ac018cf7aa653eb
emrocode <92260849+emrocode@users.noreply.github.com>
2024-06-14 13:16:22 -0400

    Add more types

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,11 +1,32 @@
 export type PreviewProps = {
   file: string;
-  fileInfo: FileList|undefined;
+  fileInfo: FileList | undefined;
   canvasRef: React.RefObject<HTMLCanvasElement>;
 };
 
 export type EditorProps = {
   handleTextChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
   option: string;
-  setOption: (option) => void;
+  setOption: (option: string) => void;
+};
+
+export type UploadProps = {
+  loading: boolean;
+  setLoading: (loading: boolean) => void;
+  handleClick: (e: React.MouseEvent<HTMLElement>) => void;
+  handleChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+};
+
+export type GalleryProps = {
+  handleClick: (e: React.MouseEvent<HTMLElement>) => void;
+};
+
+export type CardProps = {
+  image: string;
+  imageFullSize: string;
+  author: {
+    name: string;
+    profileUrl: string;
+  };
+  handleClick: (e: React.MouseEvent<HTMLElement>) => void;
 };
