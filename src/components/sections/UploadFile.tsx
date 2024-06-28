91c7dc12c760c6b2c8c6e31e33c6b95418ab7544
emrocode <92260849+emrocode@users.noreply.github.com>
2024-06-28 15:20:42 -0400

    Add dropzone

--- src/components/sections/UploadFile.tsx
+++ src/components/sections/UploadFile.tsx
@@ -1,23 +1,47 @@
+import { useDropzone } from "react-dropzone";
 import { Plus } from "lucide-react";
-import css from "../../styles/Upload.module.css";
 import Gallery from "./Gallery";
+import clsx from "clsx";
+import css from "../../styles/Upload.module.css";
 import type { UploadProps } from "../../types";
 
 const Upload: React.FC<UploadProps> = ({
-  loading,
   setLoading,
+  loading,
   handleClick,
   handleChange,
 }) => {
+  const onDrop = handleChange;
+  const { getRootProps, getInputProps, open, isDragActive } = useDropzone({
+    onDrop,
+    accept: {
+      "image/*": [".jpeg", ".png", ".gif", ".svg", ".webp"],
+    },
+    multiple: false,
+    noClick: true,
+    noKeyboard: true,
+    onFileDialogOpen: () => setLoading(true),
+    onFileDialogCancel: () => setLoading(false),
+  });
+
   return (
     <div className={css.container}>
-      <label>
-        <div className={css.upload_zone} onClick={() => setLoading(true)}>
-          <Plus size={16} />
-          <span>{loading ? "waiting..." : "open image"}</span>
+      <div className={css.upload_zone} {...getRootProps()}>
+        <div
+          className={clsx(
+            isDragActive ? [css.upload, css.active] : [css.upload],
+          )}
+        >
+          <div className={css.input_container}>
+            <input {...getInputProps()} />
+            <span>Drag 'n' drop some files here</span>
+          </div>
+          <button type="button" className={css.button} onClick={open}>
+            <Plus size={16} />
+            <span>{loading ? "waiting..." : "open image"}</span>
+          </button>
         </div>
-        <input type="file" accept="image/*" onChange={handleChange} />
-      </label>
+      </div>
       <Gallery handleClick={handleClick} />
     </div>
   );
